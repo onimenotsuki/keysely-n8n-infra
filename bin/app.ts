cd42f0b3ca60e83e983e46650d1cbb5d046d3358
Edgar Talledos <onimenotsuki@gmail.com>
2025-11-21 11:51:40 -0600

    chore: renamed stack

--- bin/app.ts
+++ bin/app.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { N8nStack } from '../lib/stacks/n8n-stack';
 
 const app = new cdk.App();
 
-new N8nStack(app, 'N8nStack', {
+// Stack is registered with app via constructor side effect
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const stack = new N8nStack(app, `keysely-n8n-stack-${process.env.CDK_DEFAULT_REGION}`, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION || 'us-west-2',
